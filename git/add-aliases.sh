48be0a179182fbec43b75737e3b34801a2e0cd76
Michael Harris <19492392+Burwin@users.noreply.github.com>
2024-02-12 10:02:55 -0500

    Create add-aliases.sh

---
+++ git/add-aliases.sh
@@ -0,0 +1,8 @@
+git config --global alias.co checkout
+git config --global alias.b branch
+git config --global alias.br branch
+git config --global alias.c commit
+git config --global alias.s status
+git config --global alias.p push
+git config --global alias.pu "push -u origin head"
+git config --global alias.undo "reset --soft HEAD~1"
