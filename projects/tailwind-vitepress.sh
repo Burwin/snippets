ce3e6027adaa5ce833e210992fc0b131f85a0b3d
Michael Harris <19492392+Burwin@users.noreply.github.com>
2024-03-01 09:55:30 -0500

    Update tailwind-vitepress.sh

--- projects/tailwind-vitepress.sh
+++ projects/tailwind-vitepress.sh
@@ -1,12 +1,54 @@
-# Create a new Vue project using Vite
-npm create vite@latest my-project -- --template vue
-cd my-project
+# gitignore vitepress
+echo "# vitepress" >> .gitignore
+echo "**/.vitepress/dist" >> .gitignore
+echo "**/.vitepress/cache" >> .gitignore
+
+# install vitepress
+npm add -D vitepress
+
+# setup vitepress
+npx vitepress init
+
+# add vue as dev dependency
+npm i -D vue
 
 # Install Tailwind CSS, PostCSS, and autoprefixer
 npm i -D tailwindcss postcss autoprefixer
 
 # Create Tailwind config and PostCSS config files
 npx tailwindcss init -p
+
+# add sources to tailwind config
+cat > tailwind.config.js <<EOF
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,vue,ts,md}", "./.vitepress/theme/**/*.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+EOF
+
+# add tailwind imports to theme css
+cat > .vitepress/theme/style.css <<EOF
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+EOF
+
+# install dependencies for TailwindUI
+npm install @headlessui/vue @heroicons/vue
+
+
+
+# Create a new Vue project using Vite
+npm create vite@latest my-project -- --template vue
+cd my-project
+
+
+
+
 
 # Replace the content of `tailwind.config.js` with the recommended configuration
 cat <<EOT > tailwind.config.js
@@ -29,13 +71,9 @@ EOT
 # install tailwind-ui
 npm install @headlessui/vue @heroicons/vue
 
-# install vitepress
-npm add -D vitepress
 
-# gitignore vitepress
-echo "# vitepress" >> .gitignore
-echo "**/.vitepress/dist" >> .gitignore
-echo "**/.vitepress/cache" >> .gitignore
+
+
 
 # setup vitepress
 npx vitepress init
