68da9f55cd3ec84c58e4dd702f4458d7ce3ee14e
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-10-21 10:35:03 -0400

    add basic install scripts

--- windows/setup-dev.ps1
+++ windows/setup-dev.ps1
@@ -1,9 +1,41 @@
-# install wsl
-# install docker desktop
-# install .net
+# install WSL
+winget install Microsoft.WSL
+
+# install Docker
+winget install Docker.DockerDesktop
+
 # install git
+winget install Git.Git
+## TODO: configure git, either with prompts or command arguments
+
 # install vscode
-# install jetbrains fonts
-# install starship
+winget install Microsoft.VisualStudioCode
+## TODO: configure, either with prompts or command arguments
+## TODO: add basic extensions
+## TODO: set default shortcuts ('code .', 'open in code...') and env vars?
+
+# install chocolatey
+winget install Chocolatey.Chocolatey
+
+# install jetbrains mono nerd-fonts
+choco install nerd-fonts-jetbrainsmono
+
+# install starship to powershell
+winget install Starship.Starship
+"Invoke-Expression (&starship init powershell)" | Out-File -FilePath $PROFILE -Encoding UTF8 -Append
+
 # install chrome
-# install IDE
+winget install Google.Chrome
+## TODO: install 1password extension?
+## TODO: configure 1password?
+## TODO: import template profile (bookmarks / settings)?
+
+# install dotnet 9
+winget install Microsoft.DotNet.SDK.9
+
+# install visual studio
+winget install Microsoft.VisualStudio.2022.Community
+## TODO: configure settings for C#
+## TODO: configure snippets
+## TODO: install NCrunch?
+## TODO: configure user profile?
