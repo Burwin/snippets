acf5d1b2613a8797847959b516f28265d5f89c30
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-11-18 17:29:19 -0500

    Create install-lazyvim.sh

---
+++ linux/install-lazyvim.sh
@@ -0,0 +1,8 @@
+# INSTALL LAZYVIM
+
+# install neovim
+sudo snap install nvim --classic
+
+# install [fzf](https://github.com/junegunn/fzf)
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
