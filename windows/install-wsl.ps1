3676831bcae43bfbcb62bb45ec06536954dfad65
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-11-18 17:07:48 -0500

    Create install-wsl.ps1

---
+++ windows/install-wsl.ps1
@@ -0,0 +1,9 @@
+# Must run as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isAdmin) {
+    Write-Error "This script must be run as Administrator."
+    exit 1
+}
+
+winget install Microsoft.WSL
+wsl install Ubuntu
