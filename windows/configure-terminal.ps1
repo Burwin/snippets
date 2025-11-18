a4e825597fbe65cc7a5cd4c0994dc9abd571b80b
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-11-18 17:16:52 -0500

    Create configure-terminal.ps1

---
+++ windows/configure-terminal.ps1
@@ -0,0 +1,16 @@
+# Must run as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isAdmin) {
+    Write-Error "This script must be run as Administrator."
+    exit 1
+}
+
+# install chocolatey
+winget install Chocolatey.Chocolatey
+
+# install jetbrains mono nerd-fonts
+choco install nerd-fonts-jetbrainsmono
+
+# install starship to powershell
+winget install Starship.Starship
+"Invoke-Expression (&starship init powershell)" | Out-File -FilePath $PROFILE -Encoding UTF8 -Append
