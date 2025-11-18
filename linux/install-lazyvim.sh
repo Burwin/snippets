6d399688feea64834dc47eba250be48f080c7785
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-11-18 17:38:35 -0500

    Update install-lazyvim.sh

--- linux/install-lazyvim.sh
+++ linux/install-lazyvim.sh
@@ -6,3 +6,5 @@ sudo snap install nvim --classic
 # install [fzf](https://github.com/junegunn/fzf)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
+
+# todo... https://lazyvim-ambitious-devs.phillips.codes/course/chapter-1/#_linux
