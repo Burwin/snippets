5b44beffa94b5bd363dfb4646be891495fa2ec86
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-11-18 16:53:40 -0500

    Delete windows/setup-nvim.ps1

--- windows/setup-nvim.ps1
+++
@@ -1,6 +0,0 @@
-# INSTALL NEOVIM
-git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
-
-# ADD CONFIGURATION HERE...
-# 1 - configure dotfiles git repo on this machine and update with remote files
-# 2 - run other configurations, as needed
