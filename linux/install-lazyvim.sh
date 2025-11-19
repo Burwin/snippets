d199b5597fd8d68adea1bf7b496727b4570c6bd3
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-11-19 08:42:08 -0500

    install lazygit

--- linux/install-lazyvim.sh
+++ linux/install-lazyvim.sh
@@ -7,4 +7,12 @@ sudo snap install nvim --classic
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
+# install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit.tar.gz
+rm lazygit
+
 # todo... https://lazyvim-ambitious-devs.phillips.codes/course/chapter-1/#_linux
