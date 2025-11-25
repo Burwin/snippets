93400295046f0768cebd98c5704cfbe735af41ab
Michael Harris <19492392+Burwin@users.noreply.github.com>
2025-11-25 09:34:51 -0500

    install stow

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,3 +1,10 @@
+# install packages
+sudo pacman -S stow
+
+# configure stow
+# TODO
+
+### most of these are setup automatically by omarchy... skip for now
 # install curl
 # install/configure terminal
 # install 1password
