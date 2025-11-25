ccd73c8229c90342c2f8e926a0b7c00ca150c8a8
Michael Harris <michael@bamboo.dev>
2025-11-25 15:19:28 -0500

    configure stow with stowrc file

--- linux/setup.sh
+++ linux/setup.sh
@@ -2,12 +2,14 @@
 sudo pacman -S stow
 
 # configure stow
-# TODO
+cp .stowrc ~/.stowrc
 
 ##TODO: make sure ssh service is installed and configured
 echo "make sure ssh-agent.service is defined/copied into systemd before running this..."
 systemctl --user daemon-reload
 systemctl --user enable --now ssh-agent
+
+##TODO: clone dotfiles
 
 ### most of these are setup automatically by omarchy... skip for now
 # install curl
