76156274247a798a59f528f67f1381ea2cb3331c
Michael Harris <michael@bamboo.dev>
2025-12-18 15:25:27 -0500

    add alias for git pull --rebase

--- git/add-aliases.sh
+++ git/add-aliases.sh
@@ -3,6 +3,8 @@ git config --global alias.b branch
 git config --global alias.br branch
 git config --global alias.c commit
 git config --global alias.s status
-git config --global alias.p push
+git config --global alias.P push
+git config --global alias.p pull
+git config --global alias.pr "pull --rebase"
 git config --global alias.pu "push -u origin head"
 git config --global alias.undo "reset --soft HEAD~1"
